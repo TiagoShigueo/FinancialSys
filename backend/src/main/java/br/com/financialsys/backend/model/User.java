3697099dbe3c779d2d9a2e2c264e9bdc2caada7e
TiagoShigueo <tiago.yamasaki@gmail.com>
2025-03-20 17:52:42 -0300

    [ADD] Model básico do usuário com banco de dados

---
+++ backend/src/main/java/br/com/financialsys/backend/model/User.java
@@ -0,0 +1,52 @@
+package br.com.financialsys.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUser;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    public User(Long idUser, String name, String password) {
+        this.idUser = idUser;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
