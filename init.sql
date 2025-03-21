8f2b4f328f340ff23670b20ddf54a08ce5e18b6d
TiagoShigueo <tiago.yamasaki@gmail.com>
2025-03-21 17:17:00 -0300

    [ADD] Script de criação da tabela dos usuários no banco de dados

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+  idUser int NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (name, password) VALUES ("Tiago", "123456");
