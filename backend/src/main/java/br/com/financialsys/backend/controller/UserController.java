784d9f2baf538e6681a33f94f285435f1c9109fb
TiagoShigueo <tiago.yamasaki@gmail.com>
2025-03-24 17:16:51 -0300

    [FIX] Alteração do tipo da requisição para criação de novos usuários. Agora utiliza o RequestBody e espera um ResponseEntity para seguir as boas práticas

--- backend/src/main/java/br/com/financialsys/backend/controller/UserController.java
+++ backend/src/main/java/br/com/financialsys/backend/controller/UserController.java
@@ -3,8 +3,10 @@ package br.com.financialsys.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +21,8 @@ public class UserController {
     private final UserService userService = new UserService();
 
     @PostMapping("/createUser")
-    public User createUser(@RequestParam String name, @RequestParam String password) {
-        return userService.createUser(null, name, password);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
     }
 
     @GetMapping("/getUsers")
