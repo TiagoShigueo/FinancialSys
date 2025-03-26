c722328282acd651ace6abcd2dc28f6e0b6362a0
TiagoShigueo <tiago.yamasaki@gmail.com>
2025-03-26 17:21:10 -0300

    [FIX] Nome da coluna do id usuário

--- backend/src/main/java/br/com/financialsys/backend/model/User.java
+++ backend/src/main/java/br/com/financialsys/backend/model/User.java
@@ -20,6 +20,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
     private Long idUser;
 
     @Column(name = "name")
