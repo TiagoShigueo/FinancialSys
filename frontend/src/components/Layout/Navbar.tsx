a40c0d981ecbc3f03c99f8628080e7c8e64a99e3
TiagoShigueo <tiago.yamasaki@gmail.com>
2025-04-24 21:58:52 -0300

    [ADD] Navbar

--- frontend/src/components/Layout/Navbar.tsx
+++ frontend/src/components/Layout/Navbar.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import Cookies from "js-cookie";
+import { logout } from "@/services/auth";
+import { useRouter } from "next/navigation";
+import { decodeJwt } from "jose";
+import { useEffect, useState } from "react";
+
 export default function Navbar() {
+  const router = useRouter();
+  const [username, setUsername] = useState<string>("");
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      try {
+        const user = decodeJwt(token);
+        if (user.sub) {
+          setUsername(String(user.sub));
+        }
+      } catch (error) {
+        console.error("Erro ao decodificar o token: " + error);
+      }
+    }
+  }, []);
+
+  const handleLogout = async () => {
+    logout();
+    router.push("/login");
+    // Quem sabe no futuro eu redirecione para a página principal
+  };
+
   return (
-    <nav className="w-full h-10  bg-blue-500">
-      <h1>Olá, seja bem vindo</h1>
+    <nav className="w-full h-14 bg-gray-900 px-6 flex items-center justify-between text-white">
+      <h1 className="text-lg">Olá {username}</h1>
+      <button
+        onClick={handleLogout}
+        className="bg-blue-600 text-white font-medium py-1 px-4 rounded hover:bg-blue-300 transition"
+      >
+        Logout
+      </button>
     </nav>
   );
 }
