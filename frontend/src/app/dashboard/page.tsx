0b9d03f597fcce08ecaab8003b4997c8ea0f11df
TiagoShigueo <tiago.yamasaki@gmail.com>
2025-04-24 22:40:38 -0300

    [ADD] Desgin inicial da tela de dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -24,21 +24,30 @@ export default function Dashboard() {
 
   return (
     <div className="min-h-screen">
-      <h1>Página de Dashboard</h1>
-
-      <button type="submit" onClick={handleLogout}>
-        Logout
-      </button>
+      <h1 className="text-center font-bold text-xl justify-center">
+        Patrimônio
+      </h1>
       <div className="flex justify-center">
         {balances.map((balance) => (
           <div
             key={balance.bankId}
-            className="p-2 m-2 text-center space-x-2 border-2 rounded-sm"
+            className="p-2 m-2 w-40 text-center space-x-2 border-2 rounded-sm"
           >
             <div>{balance.bankName}</div>
             <div> R$ {balance.balance}</div>
           </div>
         ))}
+      </div>
+      <div className="pt-4 flex">
+        <div className="border-2 h-128 w-2/6">
+          <h1 className="text-center font-bold text-xl">Entradas</h1>
+        </div>
+        <div className="border-2 h-128 w-2/6 mx-4">
+          <h1 className="text-center font-bold text-xl">Saídas</h1>
+        </div>
+        <div className="border-2 h-128 w-2/6">
+          <h1 className="text-center font-bold text-xl">Cartões</h1>
+        </div>
       </div>
     </div>
   );
