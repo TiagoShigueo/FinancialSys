08c7a8d359a843526ae5c555aff668d010ba78cf
TiagoShigueo <tiago.yamasaki@gmail.com>
2025-06-16 14:22:47 -0300

    [DEL] Permissão de PUT e DEL

--- backend/src/main/java/br/com/financialsys/backend/security/SecurityConfig.java
+++ backend/src/main/java/br/com/financialsys/backend/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
