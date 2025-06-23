7c33536c2e8b2e51f577d90233ddaf1577a6b14b
TiagoShigueo <tiago.yamasaki@gmail.com>
2025-06-23 18:22:57 -0300

    [ADD] Função para obter a role do usuário

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,3 +1,4 @@
+import { jwtVerify } from "jose";
 import Cookies from "js-cookie";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -14,11 +15,41 @@ export const login = async (username: string, password: string) => {
   }
 
   const token = await response.text();
-  localStorage.setItem("token", token);
+  // localStorage.setItem("token", token);
   return token;
 };
 
 export const logout = () => {
   Cookies.remove("token");
-  localStorage.removeItem("token");
+  // localStorage.removeItem("token");
+};
+
+function parseJwt(token: string) {
+  try {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map((c) => {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+
+    return JSON.parse(jsonPayload);
+  } catch (error) {
+    return null;
+  }
+}
+
+export const getRole = () => {
+  const token = Cookies.get("token");
+  if (!token) return null;
+
+  const payload = parseJwt(token);
+  const role = payload?.role || null;
+
+  console.log("Role:", role);
+  return role;
 };
